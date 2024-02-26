5b3068b4cd98cfa470d18ad5407d727efda5c488
AidenWalmer <aidenjwalmer@gmail.com>
2024-02-26 01:06:11 -0500

    quick fix

--- example/example.py
+++ example/example.py
@@ -1,13 +1,13 @@
 ### Code Examples (Also listed in README file):
 
 #1. Addition and Subtraction
-# 3 add 3 slice 2
-# 6.0
+#Wals > 3 add 3 slice 2
+#Wals > 6.0
 
 #2. Multiplication and Division
-#10 mix 2 fold 5
-#4.0
+#Wals > 10 mix 2 fold 5
+#Wals > 4.0
 
 #3. Handling Decimal Numbers (Float)
-#2.5 mix 3.5 slice 3 
-#5.75
+#Wals > 2.5 mix 3.5 slice 3 
+#Wals > 5.75
